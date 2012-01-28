73a1e25ccaf7c088b686004927dc8b29bd710edc
sadmicrowave <corey.m.farmer@gmail.com>
2012-01-27 22:37:22 -0600

    first project commit
    establishing project repo baseline

---
+++ TimeBlogger/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimeBlogger
+//
+//  Created by Corey Farmer on 1/27/12.
+//  Copyright self 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
